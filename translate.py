9d41f283a096f83c3364c23f0054734fb16d1af0
senayaydemir <senayaydemir00@gmail.com>
2025-05-28 09:18:40 +0300

    Çeviri fonksiyonları yazıldı

--- translate.py
+++ translate.py
@@ -3,3 +3,8 @@ import translate
 def cevir():
     metin =str(input("ÇEVİRMEK İSTEDİĞİNİZ METNİ GİRİN:"))
     dil=int(input("Hangi Dile Çevirmek İstiyorsunuz?\n1-Korece\n2-Özbekçe\n3-Japonca"))
+
+    if dil ==1:
+        translator=translate.Translator(from_lang="tr",to_lang="ko")
+        translation=translator.translate(metin)
+        print(translation)
